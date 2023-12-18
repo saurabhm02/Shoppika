1cfdb00afd99abaa960ab86256c6277e477fd9a4
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-12-19 00:52:27 +0530

    add rating portion in a product

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa6';
 
 const Product = ({ product }) => {
   const actualPrice = (product.price * 100) / (100 - product.discountPercentage);
 
   return (
-    <div className="max-w-[240px] group hover:shadow-xl transition-all duration-2000 ease-in-out">
-      <div className="img">
+    <div className="max-w-[240px] group hover:shadow-xl transition-all duration-2000 ease-in-out relative">
+      <div className="img relative">
         <img 
           src={product.thumbnail} 
           alt="product image" 
           style={{ height: '220px', width: '230px' }} 
           className=""
         />
+        <div className="p-1 absolute bottom-2 left-2 bg-white bg-opacity-70  text-xs font-semibold rounded z-50 flex items-center">
+          {product.rating}  <span className="pl-1 pr-1 text-sm text-green-600"><FaStar/></span>  | {product.ratingPeople}
+        </div>
       </div>
       <div className="content text-left flex flex-col py-2 px-2">
         <p className="font-semibold text-lg mb-1">{product.brand}</p>
